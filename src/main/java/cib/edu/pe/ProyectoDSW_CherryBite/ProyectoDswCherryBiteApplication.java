d043acc197224f016dcee529f9ba24867b0b0d87
AlexGVP <i202122156@cibertec.edu.pe>
2024-07-05 13:51:58 -0500

    Dependencias

--- src/main/java/cib/edu/pe/ProyectoDSW_CherryBite/ProyectoDswCherryBiteApplication.java
+++ src/main/java/cib/edu/pe/ProyectoDSW_CherryBite/ProyectoDswCherryBiteApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ProyectoDswCherryBiteApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(ProyectoDswCherryBiteApplication.class, args);
 	}
 
--- src/main/java/cib/edu/pe/ProyectoDSW_CherryBite/util/DtoUtil.java
+++ src/main/java/cib/edu/pe/ProyectoDSW_CherryBite/util/DtoUtil.java
@@ -1,4 +1,18 @@
 package cib.edu.pe.ProyectoDSW_CherryBite.util;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+import cib.edu.pe.ProyectoDSW_CherryBite.model.bd.Alimento;
+import cib.edu.pe.ProyectoDSW_CherryBite.model.dto.DtoEntity;
+
+@Component
 public class DtoUtil {
+
+    public DtoEntity convertirADto (Object obj, DtoEntity mapper) {
+        return new ModelMapper().map(obj, mapper.getClass());
+    }
+
+    public Object convertirDtoAEntity (Object obj, DtoEntity mapper){
+        return new ModelMapper().map(mapper,obj.getClass());
+    }
 }
